aeaaddf81586cd9178a5233c544d43ac2852065b
nguyenlongqbtt <nguyenlongqbtt@gmail.com>
2023-03-14 17:29:00 +0700

    test click url

--- frontend/src/components/Loading.tsx
+++ frontend/src/components/Loading.tsx
@@ -3,7 +3,12 @@ import React from "react";
 const Loading = ({ handelCancel }: { handelCancel: any }) => {
   return (
     <div>
-      <div className="flex justify-center mt-32">Vui lòng đợi</div>
+      {/* <div className="flex justify-center mt-32">Vui lòng đợi, trong lúc đợi thì nhấn vào link giúp adm có chi phí duy trì nhé: https://www.facebook.com/nguyenlongqblt </div> */}
+      <div className="flex justify-center mt-32">Vui lòng đợi, trong lúc đợi thì nhấn vào link shopee giúp Admin có chi phí duy trì nhé
+      </div>
+      <div className="flex justify-center mt-32">
+      <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">link shopee </a>
+      </div>
       <div className="flex justify-center items-center h-20">
         <div className="lds-ellipsis">
           <div></div>
